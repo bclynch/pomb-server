58d172701dd92bf7de9d97cc7d312eed231b4e0c
bclynch <bclynch7@gmail.com>
2018-01-31 10:02:37 -0800

    postgraphql v4

--- server.js
+++ server.js
@@ -9,8 +9,25 @@ app.set('port', process.env.PORT || 8080);
 app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.use(express.static("www")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
 //https://github.com/postgraphql/postgraphql/blob/d4fd6a4009fea75dbcaa00d743c985148050475e/docs/library.md
+
+/**
+ * PostgraphQL
+ */
+const pgConnection = {
+  host: 'localhost',
+  user: 'pomb_admin',
+  password: 'abc123',
+  database: 'bclynch',
+  port: 5432
+};
+const postgraphqlConfig = {
+  graphiql: true,
+  jwtSecret: 'some-secret', 
+  jwtPgTypeIdentifier: 'pomb.jwt_token'
+};
+app.use(postgraphql(pgConnection, ['pomb','pomb_private'], postgraphqlConfig));
 // app.use(postgraphql('postgresql://laze_anonymous:abc123@laze.c0up3bfsdxiy.us-east-1.rds.amazonaws.com:5432/laze?sslmode=require&ssl=1', ['laze','laze_private'], {graphiql: true, jwtSecret: new Buffer('some-secret', 'base64'), jwtPgTypeIdentifier: 'laze.jwt_token'})); // pgDefaultRole: 'bclynch'
-app.use(postgraphql('postgres://pomb_admin:abc123@localhost:5432/bclynch', ['pomb','pomb_private'], {graphiql: true, jwtSecret: 'some-secret', jwtPgTypeIdentifier: 'pomb.jwt_token'}));
+// app.use(postgraphql('postgres://pomb_admin:abc123@localhost:5432/bclynch', ['pomb','pomb_private'], {graphiql: true, jwtSecret: 'some-secret', jwtPgTypeIdentifier: 'pomb.jwt_token'}));
 
 //routes
 app.use('/upload-images', require('./imageProcessing'));
